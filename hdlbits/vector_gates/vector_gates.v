c75fdac2ce81aef56b87368d458050bfd24f97a1
sprsr <projecthourglass@protonmail.com>
2023-09-04 08:20:54 -0700

    smart logic vector gates

--- hdlbits/vector_gates/vector_gates.v
+++ hdlbits/vector_gates/vector_gates.v
@@ -7,11 +7,6 @@ module vector_gates(
 );
     assign out_or_bitwise = (a | b);
     assign out_or_logical = (a || b);
-    assign out_not[5]     = !b[2];
-    assign out_not[4]     = !b[1];
-    assign out_not[3]     = !b[0];
-    assign out_not[2]	  = !a[2];
-    assign out_not[1]	  = !a[1];
-    assign out_not[0]	  = !a[0];
-
+    assign out_not[2:0] = ~a;	
+	assign out_not[5:3] = ~b;
 endmodule
