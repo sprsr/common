639af75da3ba1605221e34216d4ca16a7d3b6ac1
sprsr <projecthourglass@protonmail.com>
2023-09-07 19:18:30 -0500

    module shift

---
+++ hdlbits/module_shift/module_shift.v
@@ -0,0 +1,17 @@
+module module_shift( input clk, input d, output q );
+    wire w1;
+    wire w2;
+    my_dff d1 ( .clk(clk),
+           		.d(d),
+           		.q(w1)
+          	  );
+    my_dff d2 ( .clk(clk),
+               .d(w1),
+               .q(w2)
+          	  );
+    my_dff d3 ( .clk(clk),
+               .d(w2),
+               .q(q)
+              );
+  
+endmodule
